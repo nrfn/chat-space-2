7494f7ee8d3558cd4c5e74c86c8f522f228205ab
nrfn <59220732+nrfn@users.noreply.github.com>
2020-03-22 16:25:57 +0900

    reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,7 +10,7 @@ $(function(){
           ${message.created_at}
         </div>
       </div>
-      <div class="main__messages__message">
+      <div class="main__messages__message", data-message-id=${message.id}>
         ${message.content}
         <br>
         <img src="${message.image}" >
@@ -26,10 +26,9 @@ $(function(){
           ${message.created_at}
         </div>
       </div>
-      <div class="main__messages__message">
+      <div class="main__messages__message", data-message-id=${message.id}>
         ${message.content}
       </div>`
-      console.log("OK");
       return html;
     };
   }
@@ -57,4 +56,31 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main__messages__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main__messages').append(insertHTML);
+        $('.main__messages').animate({ scrollTop: $('.main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if(document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
